0a9b47cad8603e80076274c6d1d61647674beb8f
devsrabon <et.srabon@gmail.com>
2022-10-12 12:07:52 +0600

    added added toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Question = ({ data }) => {
-	const { id, question, options, correctAnswer } = data;
+	const { question, options, correctAnswer } = data;
+	// console.log(typeof correctAnswer, )
+	const handleClick = (id) =>{
+		if (correctAnswer === id) {
+			toast.success("Answer is correct!", { autoClose: 2000 })
+		}
+		else {
+			toast.error('Wrong Answer!', { autoClose: 2000 });
+		}
+	}
 	
 	return (
 		<div className="container text-center bg-light shadow-lg my-5">
@@ -9,15 +19,45 @@ const Question = ({ data }) => {
 			<div className="d-flex  gap-5">
 				<div className="col gap-4">
 					<div className="p-3 bg-info rounded my-4">
-						<label className="fs-5 ms-2" for={options[0]}>
-							<input type="radio" id={options[0]} name="age" value="30" />
+						<label
+							className="d-flex justify-content-center gap-1 fs-6 ms-2"
+							for={options[0]}
+						>
+							<input
+								onClick={() => handleClick(options[0])}
+								type="radio"
+								id={options[0]}
+								name="age"
+								value="30"
+							/>
+							<ToastContainer
+								position="top-center"
+								autoClose={2000}
+								hideProgressBar={false}
+								newestOnTop={false}
+								closeOnClick
+								rtl={false}
+								pauseOnFocusLoss
+								draggable
+								pauseOnHover
+								theme="light"
+							/>
 							{options[0]}
 						</label>
 					</div>
 					<div className="p-3 bg-info rounded my-4">
 						<div>
-							<label className="fs-5 ms-2" for={options[1]}>
-								<input type="radio" id={options[1]} name="age" value="40" />
+							<label
+								className="d-flex justify-content-center gap-1 fs-6 ms-2"
+								for={options[1]}
+							>
+								<input
+									onClick={() => handleClick(options[1])}
+									type="radio"
+									id={options[1]}
+									name="age"
+									value="40"
+								/>
 								{options[1]}
 							</label>
 						</div>
@@ -25,15 +65,33 @@ const Question = ({ data }) => {
 				</div>
 				<div className="col gap-4">
 					<div className="p-3 bg-info rounded my-4">
-						<label className="fs-5 ms-2" for={options[2]}>
-							<input type="radio" id={options[2]} name="age" value="50" />
+						<label
+							className="d-flex justify-content-center gap-1 fs-6 ms-2"
+							for={options[2]}
+						>
+							<input
+								onClick={() => handleClick(options[2])}
+								type="radio"
+								id={options[2]}
+								name="age"
+								value="50"
+							/>
 							{options[2]}
 						</label>
 					</div>
 					<div className="p-3 bg-info rounded ">
 						<div>
-							<label className="fs-5 ms-2" for={options[3]}>
-								<input type="radio" id={options[3]} name="age" value="60" />
+							<label
+								className="d-flex justify-content-center gap-1 fs-6 ms-2"
+								for={options[3]}
+							>
+								<input
+									onClick={() => handleClick(options[3])}
+									type="radio"
+									id={options[3]}
+									name="age"
+									value="60"
+								/>
 								{options[3]}
 							</label>
 						</div>
